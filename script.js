2dc05fee5bb91fadc1f16a444174af1b8ea39015
Chrisvagery <112591663+Chrisvagery@users.noreply.github.com>
2024-02-24 01:06:09 -0300

    oi

--- script.js
+++ script.js
@@ -4,14 +4,43 @@ document.getElementById("addButton").addEventListener("click", function () {
 
   if (taskText !== "") {
     var taskList = document.getElementById("taskList");
-    var li = document.createElement("li");
-    li.innerHTML = taskText + ' <button class="deleteButton">X</button>';
-    taskList.appendChild(li);
+    taskList.appendChild(createElementLi(taskText));
 
     taskInput.value = "";
 
-    li.querySelector(".deleteButton").addEventListener("click", function () {
-      taskList.removeChild(li);
-    });
+    var lista = JSON.parse(localStorage.getItem("tarefas")) || [];
+    lista.push(taskText)
+    localStorage.setItem("tarefas", JSON.stringify(lista));
   }
 });
+
+
+window.addEventListener('DOMContentLoaded', function() {
+  var lista = JSON.parse(localStorage.getItem("tarefas")) || [];
+  var taskList = document.getElementById("taskList");
+  console.log(lista);
+
+  lista.forEach(function(item) {
+    taskList.appendChild(createElementLi(item));
+  })
+})
+
+
+function createElementLi(item) {
+  var li = document.createElement("li");
+  li.innerHTML = item + ' <button class="deleteButton">X</button>';
+  li.querySelector(".deleteButton").addEventListener("click", function () {
+    taskList.removeChild(li);
+
+    var removeElemento = JSON.parse(localStorage.getItem("tarefas")) || [];
+    var index = removeElemento.indexOf(item);
+    if (index !== -1) {
+      removeElemento.splice(index, 1);
+      localStorage.setItem("tarefas", JSON.stringify(removeElemento));
+    }
+    
+    taskList.removeChild(li);
+    
+  });
+  return li
+}
